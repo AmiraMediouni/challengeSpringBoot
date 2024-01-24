894a8ed22244771bebf5b39162cef3c5d3179b22
amira_mediouni <amira.mediouni@hotmail.com>
2024-01-24 14:57:00 +0100

    challenge1

---
+++ src/main/java/com/example/challenge/controllers/Home.java
@@ -0,0 +1,12 @@
+package com.example.challenge.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+    @GetMapping(path="${apiPrefix}/welcome")
+    public static void helloWorld(){
+        System.out.println("hello world");
+    }
+}
