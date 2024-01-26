44f94fdc0fa352ec81cb4b828dbc856363761b49
amira_mediouni <amira.mediouni@hotmail.com>
2024-01-26 10:54:50 +0100

    challenge2_avec_erreur

--- src/main/java/com/example/challenge/ChallengeApplication.java
+++ src/main/java/com/example/challenge/ChallengeApplication.java
@@ -4,11 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.sql.*;
+
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ChallengeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChallengeApplication.class, args);
-    }
+        try{
+            Connection connexion = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/myDatabase",
+                    "root",
+                    "database2023");
+            Statement statement=connexion.createStatement();
+            ResultSet resultSet=statement.executeQuery("select * from customers");
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("firstName"));
+                System.out.println(resultSet.getString("lastName"));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+
+         }
 
 }
