f3074b257ecdc91e21dcaa7e721eb17082aacf74
amira_mediouni <amira.mediouni@hotmail.com>
2024-02-05 15:01:27 +0100

    challenge4

--- src/main/java/com/example/challenge/ChallengeApplication.java
+++ src/main/java/com/example/challenge/ChallengeApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.sql.*;
 
